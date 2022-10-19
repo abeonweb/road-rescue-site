443cd7b38b2e46b91048f2d967aaa4572c471987
abeonweb <bmospecific@yahoo.com>
2022-10-19 05:40:39 +0100

    modified:   src/App.css 	modified:   src/components/Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,8 @@ import "animate.css"
 function Hero() {
     return (
         <section className="hero-container">
-            <div className="hero overlay">
+            <div className="overlay"></div>
+            <div className="hero">
                 <div className="logo-title-container">
                 {/* <img className="page-logo" src={logo} alt="road rescue logo" /> */}
                     <h1 className="hero__title">
@@ -16,13 +17,13 @@ function Hero() {
 
                 <div className="catchphrase-links-container">
                     <h2 className="hero__phrase">Fix road defects fast</h2>
-                    {/* <p className="hero__tagline">Solving road issues should not be an issue. Let us help out.</p> */}
-                    <ul className="hero__tagline">
+                    <p className="hero__tagline">Solving road issues should not be an issue. Let us help out.</p>
+                    {/* <ul className="hero__tagline">
                         <li className="hero__tagline-list">The number one solution to fix roads fast</li>
                         <li className="hero__tagline-list">100% reliance</li>
                         <li className="hero__tagline-list">Save money on repairs </li>
                         <li className="hero__tagline-list">Trustworthy even in harsh conditions</li>
-                    </ul>
+                    </ul> */}
                 </div>
                 <div className="cta-container">
                     <Link to="/road-rescue-site/#form" className="hero__cta hero__cta--form animate__animated animate__slideInLeft shadow">Get a quote</Link>
