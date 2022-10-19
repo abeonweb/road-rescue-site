53e94691bcd1bc194c56bef84d6c74f23eca1aad
abeonweb <bmospecific@yahoo.com>
2022-10-19 12:29:07 +0100

    modified:   src/App.css 	modified:   src/components/Hero.js 	modified:   src/components/Navbar.js 	modified:   src/css/Navbar.css 	new file:   src/images/check-mark.png 	deleted:    src/images/road-network.jpg 	deleted:    src/images/rock.png

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,30 +5,33 @@ import "animate.css"
 function Hero() {
     return (
         <section className="hero-container">
-            <div className="hero overlay">
-                <div className="logo-title-container">
-                {/* <img className="page-logo" src={logo} alt="road rescue logo" /> */}
-                    <h1 className="hero__title">
-                        <span className="hero__title--primary">R</span>oad <span className="hero__title--primary">R</span>escue
-                        <span className="hero__title--secondary">& Maintenance company limited</span>
-                    </h1>
-                </div>
+            <div className="overlay"></div>
+            <div className="hero">
 
-                <div className="catchphrase-links-container">
+                <div className="catchphrase-container">
                     <h2 className="hero__phrase">Fix road defects fast</h2>
                     {/* <p className="hero__tagline">Solving road issues should not be an issue. Let us help out.</p> */}
                     <ul className="hero__tagline">
-                        <li className="hero__tagline-list">The number one solution to fix roads fast</li>
-                        <li className="hero__tagline-list">100% reliance</li>
+                        <li className="hero__tagline-list">#1 solution to fix roads fast</li>
                         <li className="hero__tagline-list">Save money on repairs </li>
-                        <li className="hero__tagline-list">Trustworthy even in harsh conditions</li>
+                        <li className="hero__tagline-list">Can be applied even in harsh conditions</li>
                     </ul>
+                    <div className="cta-container">
+                        <Link to="/road-rescue-site/#form" className="hero__cta-btn hero__cta-btn-form animate__animated animate__slideInLeft shadow">Get a quote</Link>
+                        <Link to="/road-rescue-site/#contact" className="hero__cta-btn hero__cta-btn-contact animate__animated animate__slideInRight shadow">Call us</Link>
+                    </div>
                 </div>
-                <div className="cta-container">
-                    <Link to="/road-rescue-site/#form" className="hero__cta hero__cta--form animate__animated animate__slideInLeft shadow">Get a quote</Link>
-                    <Link to="/road-rescue-site/#contact" className="hero__cta hero__cta--contact animate__animated animate__slideInRight shadow">Call us</Link>
+                <div>
+
+                    <div className="logo-title-container">
+                        {/* <img className="page-logo" src={logo} alt="road rescue logo" /> */}
+                        <h1 className="hero__title">
+                            <span className="hero__title--primary">Road Rescue</span>
+                            <span className="hero__title--secondary">& Maintenance company limited</span>
+                        </h1>
+                    </div>
                 </div>
-                
+
             </div>
         </section>
     )
