0b6d015e4eecc4b1ec100af14d44bfea14a739b7
abeonweb <bmospecific@yahoo.com>
2022-11-20 08:45:45 +0100

    redo of the product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -5,18 +5,16 @@ export default function Product() {
     return (
         <main className="main">
             <h1 className="page--title">Cold Mix Asphalt</h1>
-            <section className="section section__product">
+            <section className="section section-product">
                 <h2 className="page--subtitle">Learn some more about the our asphalt.</h2>
-                <section className="section__product--features">
+                <section className="section-product-features">
                     {
                         productFeatures.map((feature, index) => {
                             const { id, image, heading, body } = feature
                             return (
-                                <div key={id} className={`product--feature ${index % 2 === 0 ? "right" : "left"}`}>
-                                    <div className="image-container">
-                                        <img className={`image`} src={require("../images/" + image + ".png")} alt="" />
-                                    </div>
-                                    <div className="info">
+                                <div key={id} className={`product-feature ${index % 2 === 0 ? "right" : "left"}`}>
+                                    <img className="product-image" src={require("../images/" + image + ".png")} alt="" />
+                                    <div className="product-info">
                                         <h5 className="heading" >{heading}</h5>
                                         <p className="body" >{body}</p>
                                     </div>
