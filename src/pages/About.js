24c59bf27f3d0d3ff3f6734d9b6f512a3506893a
abeonweb <bmospecific@yahoo.com>
2022-11-24 18:12:07 +0100

    expand/collapse text added to about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,14 +20,16 @@ export default function About() {
                 <article className="objective">
                     {/* <h2 className="section--subtitle about-subtitle">Youth Empowerment</h2> */}
                     <div className="objective-container">
+                        {/* <div className="about-image-youth"></div> */}
                         <img src={youthEmpowerment} alt="threa people at a desk" className="about-image" />
                         <div>
                             <h3 className="about-body-title">Vision,  New Ideas. Innovation that can make a difference.</h3>
-                            <p className="about-body">We believe in the employment of the young and vibrant workforce. Nigeria has a very young
+                            <p className="about-body cutoff-text">We believe in the employment of the young and vibrant workforce. Nigeria has a very young
                                 population. With the average age of the population at around 18 years old, it is our strong
                                 belief that empowering the youth directly through employment opportunities in the sector through
                                 our endeavour at Road Rescue and Maintenance in conjunction with a well meaning Public sector can
                                 bring about positive change. </p>
+                                <input type="checkbox" className="expand-btn"/>
                         </div>
                     </div>
                 </article>
@@ -35,12 +37,19 @@ export default function About() {
                 <article className="objective">
                     {/* <h2 className="section--subtitle about-subtitle">Economic Growth</h2> */}
                     <div className="objective-container">
+                        {/* <div className="about-image-growth"></div> */}
                         <img src={financialGrowth} alt="economic growth graphic" className="about-image" />
                         <div>
                             <h3 className="about-body-title">Proper infrastructure leads to exponential economic growth</h3>
-                            <p className="about-body">There is also the indirect impact of those works
-                                through the improvement of road infrastructure which in turn will be crucial to economic growth at
-                                local, state and National level in due time through the cascade effect.</p>
+                            <p className="about-body cutoff-text">There is also the indirect impact of large scale infrastructure 
+                                development on the economy. At Road Rescue Maintenance Company Limited our goal is to empower a portion 
+                                of the working population. Unlocking the potential of more and more people to become sel employed, gainfully
+                                employed through a value chain that is spurred by this affordable and easy to use technology.
+                                
+                             The improvement of road infrastructure allowing goods and services to go from point A to point B in a timely 
+                                manner, with less loss and damage to property, which in turn will be crucial to economic growth at local, 
+                                state and National level in due time through the cascade effect.</p>
+                                <input type="checkbox" className="expand-btn"/>
                         </div>
                     </div>
                 </article>
