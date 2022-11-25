b435d239c78d7ce71ecb294d707cd82d331ad236
abeonweb <bmospecific@yahoo.com>
2022-11-25 16:50:00 +0100

    update about to use map on about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,34 @@
-import { teamData } from "../data"
+import { about, teamData, vision } from "../data"
 import "../css/About.css"
 import Profile from "../components/Profile"
-import financialGrowth from "../images/financialGrowth.webp"
-import youthEmpowerment from "../images/youthEmpowerment.webp"
+
 export default function About() {
+
+    const visionList = vision.map(({ image, alt, text }, index) => {
+        return (
+            <li key={`vision${index}`} className="vision-list-item">
+                <div className="vision-card">
+                    <img src={require("../images/" + image + ".webp")} alt={alt} className="vision-image" />
+                    <p className="vision-body">{text}</p>
+                </div>
+            </li>
+        )
+    })
+
+    const aboutSection = about.map(({ image, alt, title, body, id }) => {
+        return (
+            <article key={`objective${id}`} className="objective">
+                <div className="objective-container">
+                    <img src={require(`../images/${image}.webp`)} alt={alt} className="about-image" />
+                    <div>
+                        <h3 className="about-body-title">{title}</h3>
+                        <p className="about-body cutoff-text">{body}</p>
+                        <input type="checkbox" className="expand-btn" />
+                    </div>
+                </div>
+            </article>
+        )
+    })
 
     const managementTeam = teamData.map(executive => {
         return (
@@ -14,45 +39,15 @@ export default function About() {
     return (
         <main className="main">
             <h1 className="page--title about-title">Who we are</h1>
+            <section className="section section__vision">
+                <h2 className="section--title">Vision</h2>
+                <ul className="vision-list">
+                    {visionList}
+                </ul>
+            </section>
             <section className="section section__about">
                 <h2 className="section--title">Our Objective</h2>
-
-                <article className="objective">
-                    {/* <h2 className="section--subtitle about-subtitle">Youth Empowerment</h2> */}
-                    <div className="objective-container">
-                        {/* <div className="about-image-youth"></div> */}
-                        <img src={youthEmpowerment} alt="threa people at a desk" className="about-image" />
-                        <div>
-                            <h3 className="about-body-title">Vision,  New Ideas. Innovation that can make a difference.</h3>
-                            <p className="about-body cutoff-text">We believe in the employment of the young and vibrant workforce. Nigeria has a very young
-                                population. With the average age of the population at around 18 years old, it is our strong
-                                belief that empowering the youth directly through employment opportunities in the sector through
-                                our endeavour at Road Rescue and Maintenance in conjunction with a well meaning Public sector can
-                                bring about positive change. </p>
-                                <input type="checkbox" className="expand-btn"/>
-                        </div>
-                    </div>
-                </article>
-
-                <article className="objective">
-                    {/* <h2 className="section--subtitle about-subtitle">Economic Growth</h2> */}
-                    <div className="objective-container">
-                        {/* <div className="about-image-growth"></div> */}
-                        <img src={financialGrowth} alt="economic growth graphic" className="about-image" />
-                        <div>
-                            <h3 className="about-body-title">Proper infrastructure leads to exponential economic growth</h3>
-                            <p className="about-body cutoff-text">There is also the indirect impact of large scale infrastructure 
-                                development on the economy. At Road Rescue Maintenance Company Limited our goal is to empower a portion 
-                                of the working population. Unlocking the potential of more and more people to become sel employed, gainfully
-                                employed through a value chain that is spurred by this affordable and easy to use technology.
-                                
-                             The improvement of road infrastructure allowing goods and services to go from point A to point B in a timely 
-                                manner, with less loss and damage to property, which in turn will be crucial to economic growth at local, 
-                                state and National level in due time through the cascade effect.</p>
-                                <input type="checkbox" className="expand-btn"/>
-                        </div>
-                    </div>
-                </article>
+                {aboutSection}
             </section>
             <section className="section section__management">
                 <h2 className="section--title management-section-title">Our Management Team</h2>
@@ -60,6 +55,6 @@ export default function About() {
                     {managementTeam}
                 </div>
             </section>
-        </main>
+        </main >
     )
 }
