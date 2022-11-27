75cf33f07213abd6b490c546e9db45e705d6a88a
abeonweb <bmospecific@yahoo.com>
2022-11-27 07:33:16 +0100

    import visioncard, objective components.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,20 @@
 import { about, teamData, vision } from "../data"
 import "../css/About.css"
 import Profile from "../components/Profile"
+import VisionCard from "../components/VisionCard"
+import Objective from "../components/Objective"
 
 export default function About() {
 
-    const visionList = vision.map(({ image, alt, text }, index) => {
+    const visionList = vision.map((item, index) => {
         return (
-            <li key={`vision${index}`} className="vision-list-item">
-                <div className="vision-card">
-                    <img src={require("../images/" + image + ".webp")} alt={alt} className="vision-image" />
-                    <p className="vision-body">{text}</p>
-                </div>
-            </li>
+                <VisionCard key={`vision${index}`} {...item} />
         )
     })
 
-    const aboutSection = about.map(({ image, alt, title, body, id }) => {
+    const aboutSection = about.map((item, index) => {
         return (
-            <article key={`objective${id}`} className="objective">
-                <div className="objective-container">
-                    <img src={require(`../images/${image}.webp`)} alt={alt} className="about-image" />
-                    <div>
-                        <h3 className="about-body-title">{title}</h3>
-                        <p className="about-body cutoff-text">{body}</p>
-                        <input type="checkbox" className="expand-btn" />
-                    </div>
-                </div>
-            </article>
+            <Objective key={`objective${index}`} {...item} />
         )
     })
 
@@ -40,10 +28,10 @@ export default function About() {
         <main className="main">
             <h1 className="page--title about-title">Who we are</h1>
             <section className="section section__vision">
-                <h2 className="section--title">Vision</h2>
-                <ul className="vision-list">
+                <h2 className="section--title"> Our Vision</h2>
+                <div className="vision-list">
                     {visionList}
-                </ul>
+                </div>
             </section>
             <section className="section section__about">
                 <h2 className="section--title">Our Objective</h2>
