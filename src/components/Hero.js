a878948d154d4910f94d4cb3150962f740115191
abeonweb <bmospecific@yahoo.com>
2022-11-29 15:36:57 +0100

    added icons as images to better control placement

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { HashLink as Link } from "react-router-hash-link"
 import "animate.css"
+import checkMark from "../images/check-mark.png"
 
 function Hero() {
     return (
@@ -11,9 +12,9 @@ function Hero() {
                 <div className="catchphrase-container">
                     <h2 className="hero__phrase"><span className="hero-highlight">Fix</span> road defects <span className="hero-highlight">fast</span></h2>
                     <ul className="hero__list">
-                        <li className="hero__list-item">#1 solution to fix roads fast</li>
-                        <li className="hero__list-item">Save money on repairs </li>
-                        <li className="hero__list-item">Can be applied even in harsh conditions</li>
+                        <li className="hero__list-item"><img src={checkMark} className="hero__list-icon" alt=""/> #1 solution to fix roads fast</li>
+                        <li className="hero__list-item"><img src={checkMark} className="hero__list-icon" alt=""/> Save money on repairs </li>
+                        <li className="hero__list-item"><img src={checkMark} className="hero__list-icon" alt=""/> Can be applied even in harsh conditions</li>
                     </ul>
                     <div className="cta-container">
                         <Link to="/road-rescue-site/#form" className="hero__cta-btn hero__cta-btn-form animate__animated animate__slideInLeft shadow">Get a quote</Link>
