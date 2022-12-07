3e2fd73e4d91501acf9bc135b9986c691ab6d943
abeonweb <bmospecific@yahoo.com>
2022-12-07 08:16:19 +0100

    added policy paths

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import Contacts from "./pages/Contacts"
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import CookiePolicy from "./pages/CookiePolicy";
 import Terms from "./pages/Terms";
 import Services from "./pages/Services";
+import Success from "./pages/Success";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/road-rescue-site" element={<SharedLayout />} >
+        <Route exact path="/" element={<SharedLayout />} >
           <Route index element={<Home />} />
           <Route path="product" element={<Product />} />
           <Route path="services" element={<Services />} />
           <Route path="about" element={<About />} />
+          <Route path="success" element={<Success />} />
           <Route path="contacts" element={<Contacts />} />
           <Route path="privacy" element={<PrivacyPolicy />} />
+          <Route path="cookie-policy" element={<CookiePolicy />} />
           <Route path="terms" element={<Terms />} />
           <Route path="*" element={<Error />} />
         </Route>
